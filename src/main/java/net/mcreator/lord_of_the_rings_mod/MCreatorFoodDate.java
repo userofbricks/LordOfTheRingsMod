b30750f6b7923ba76d035259dfa92d9ed8e94df8
Userofbricks <userofbricks11@gmail.com>
2020-03-10 16:36:27 -0700

    finished block models

--- src/main/java/net/mcreator/lord_of_the_rings_mod/MCreatorFoodDate.java
+++ src/main/java/net/mcreator/lord_of_the_rings_mod/MCreatorFoodDate.java
@@ -3,23 +3,29 @@ package net.mcreator.lord_of_the_rings_mod;
 import net.minecraftforge.registries.ObjectHolder;
 
 import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.util.Rotation;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Direction;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.DirectionProperty;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
-import net.minecraft.item.BlockItem;
+import net.minecraft.item.Food;
+import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.SoundType;
+import net.minecraft.block.DirectionalBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Block;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+
+import net.mcreator.lord_of_the_rings_mod.ItemBase.PlaceableFoodItem;
 
 import java.util.List;
 import java.util.Collections;
-
-import net.mcreator.lord_of_the_rings_mod.ItemBase.PlaceableFoodItem;
-import net.mcreator.lord_of_the_rings_mod.blockBase.CustomCakeBlock;
-import net.minecraft.world.IBlockReader;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.item.Food.Builder;
-import net.minecraft.item.Food;
 
 @Elementslord_of_the_rings_mod.ModElement.Tag
 public class MCreatorFoodDate extends Elementslord_of_the_rings_mod.ModElement {
@@ -33,15 +39,60 @@ public class MCreatorFoodDate extends Elementslord_of_the_rings_mod.ModElement {
 	@Override
 	public void initElements() {
 		elements.blocks.add(() -> new CustomBlock());
-		elements.items.add(() -> new PlaceableFoodItem(block, new Item.Properties().group(MCreatorLOTRFoodAndDrinkTab.tab)
-				.food((new Food.Builder()).hunger(2).saturation(0.15f).build()), 0.0F, false)
-				.setRegistryName(block.getRegistryName()));
+		elements.items.add(() -> new PlaceableFoodItem(block, new Item.Properties().group(MCreatorLOTRFoodAndDrinkTab.tab).food(
+				(new Food.Builder()).hunger(2).saturation(0.15f).build()), 0.0F, false).setRegistryName(block.getRegistryName()));
 	}
 
 	public static class CustomBlock extends Block {
+		public static final DirectionProperty FACING = DirectionalBlock.FACING;
+		
+      protected static final VoxelShape SOUTH_AABB = Block.makeCuboidShape(6, 5, 0, 10, 11, 4);
+      protected static final VoxelShape NORTH_AABB = Block.makeCuboidShape(6, 5, 12, 10, 11, 16);
+      protected static final VoxelShape WEST_AABB = Block.makeCuboidShape(12, 5, 6, 16, 11, 10);
+      protected static final VoxelShape EAST_AABB = Block.makeCuboidShape(0, 5, 6, 4, 11, 10);
+      protected static final VoxelShape DOWN_AABB = Block.makeCuboidShape(6, 10, 6, 10, 16, 10);
+      protected static final VoxelShape UP_AABB = Block.makeCuboidShape(6, 0, 6, 10, 6, 10);
+		
 		public CustomBlock() {
 			super(Block.Properties.create(Material.CAKE).sound(SoundType.CLOTH).hardnessAndResistance(0.5f, 1f).lightValue(0));
+			this.setDefaultState(this.stateContainer.getBaseState().with(FACING, Direction.NORTH));
 			setRegistryName("fooddate");
+		}
+
+   public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+      switch((Direction)state.get(FACING)) {
+      case NORTH:
+      default:
+         return NORTH_AABB;
+      case SOUTH:
+         return SOUTH_AABB;
+      case EAST:
+         return EAST_AABB;
+      case WEST:
+         return WEST_AABB;
+      case UP:
+         return UP_AABB;
+      case DOWN:
+         return DOWN_AABB;
+      }
+   }
+
+		@Override
+		protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+			builder.add(FACING);
+		}
+
+		public BlockState rotate(BlockState state, Rotation rot) {
+			return state.with(FACING, rot.rotate(state.get(FACING)));
+		}
+
+		public BlockState mirror(BlockState state, Mirror mirrorIn) {
+			return state.rotate(mirrorIn.toRotation(state.get(FACING)));
+		}
+
+		@Override
+		public BlockState getStateForPlacement(BlockItemUseContext context) {
+			return this.getDefaultState().with(FACING, context.getFace());
 		}
 
 		@Override
