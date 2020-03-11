d23838fe31c75371c7b9eae001dd00af1b7bff09
Userofbricks <userofbricks11@gmail.com>
2020-03-10 21:01:40 -0700

    added: appleGreen appleYellow Gammon

---
+++ src/main/java/net/mcreator/lord_of_the_rings_mod/MCreatorFoodGammon.java
@@ -0,0 +1,36 @@
+package net.mcreator.lord_of_the_rings_mod;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.UseAction;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.Food;
+
+@Elementslord_of_the_rings_mod.ModElement.Tag
+public class MCreatorFoodGammon extends Elementslord_of_the_rings_mod.ModElement {
+	@ObjectHolder("lord_of_the_rings_mod:foodgammon")
+	public static final Item block = null;
+
+	public MCreatorFoodGammon(Elementslord_of_the_rings_mod instance) {
+		super(instance, 25);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new FoodItemCustom());
+	}
+
+	public static class FoodItemCustom extends Item {
+		public FoodItemCustom() {
+			super(new Item.Properties().group(MCreatorLOTRFoodAndDrinkTab.tab).maxStackSize(64)
+					.food((new Food.Builder()).hunger(4).saturation(0.3f).build()));
+			setRegistryName("foodgammon");
+		}
+
+		@Override
+		public UseAction getUseAction(ItemStack par1ItemStack) {
+			return UseAction.EAT;
+		}
+	}
+}
