a79e0ef50d8810ec5ec0098232e0c5717beb1665
Userofbricks <userofbricks11@gmail.com>
2020-03-10 21:27:38 -0700

    added: pears, and Cherries.

---
+++ src/main/java/net/mcreator/lord_of_the_rings_mod/MCreatorFoodCherriesYellow.java
@@ -0,0 +1,41 @@
+package net.mcreator.lord_of_the_rings_mod;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.UseAction;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.Food;
+
+@Elementslord_of_the_rings_mod.ModElement.Tag
+public class MCreatorFoodCherriesYellow extends Elementslord_of_the_rings_mod.ModElement {
+	@ObjectHolder("lord_of_the_rings_mod:foodcherriesyellow")
+	public static final Item block = null;
+
+	public MCreatorFoodCherriesYellow(Elementslord_of_the_rings_mod instance) {
+		super(instance, 33);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new FoodItemCustom());
+	}
+
+	public static class FoodItemCustom extends Item {
+		public FoodItemCustom() {
+			super(new Item.Properties().group(MCreatorLOTRFoodAndDrinkTab.tab).maxStackSize(64)
+					.food((new Food.Builder()).hunger(2).saturation(0.3f).build()));
+			setRegistryName("foodcherriesyellow");
+		}
+
+		@Override
+		public int getUseDuration(ItemStack stack) {
+			return 24;
+		}
+
+		@Override
+		public UseAction getUseAction(ItemStack par1ItemStack) {
+			return UseAction.EAT;
+		}
+	}
+}
